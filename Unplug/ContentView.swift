e67acecbea5c559c19d4bace8180767e6f216e0c
Thameem Hassan <thameemh@icloud.com>
2025-06-28 21:19:41 +0500

    Initial Commit

---
+++ Unplug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unplug
+//
+//  Created by Thameem Hassan on 28-6-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unplug/UnplugApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugApp.swift
+//  Unplug
+//
+//  Created by Thameem Hassan on 28-6-25.
+//
+
+import SwiftUI
+
+@main
+struct UnplugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
